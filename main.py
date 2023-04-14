0a0dd8cddcc92bdcdb993ddd50ca08a60c743d80
Gswt <gswt.code@gmail.com>
2023-04-14 22:29:03 +0700

    fix(main-program): size array user dan candi +1 untuk mark(None)

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # import modules (jin, proses, bondowoso, dst)
 
-users = [None for i in range(102)] # Array of user ([username, password, role])
+users = [None for i in range(103)] # Array of user ([username, password, role])
 # Panjang array 102 (bodowoso, roro, dan 100 jin)
-candi = [None for i in range(100)]  # Array of candi ([id, pembuat, pasir, batu, air])
+candi = [None for i in range(101)]  # Array of candi ([id, pembuat, pasir, batu, air])
 # Panjang array 100 (banyak candi maks)
 bahan_bangunan = [0,0,0] # bahan_bangunan = [<pasir>, <batu>, <air>]
 harga_candi = [0,0,0] # harga_candi = [<pasir>, <batu>, <air>]
